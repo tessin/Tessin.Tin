ca62122c0f624da1050e2d6282289754687871f7
Thomas Bladh <thomas@tessin.se>
2017-03-27 16:19:00 +0200

    More tests for Tin class.

--- Tessin.Tin.Tests/TinTests.cs
+++ Tessin.Tin.Tests/TinTests.cs
@@ -6,7 +6,14 @@ namespace Tessin.Tin.Tests
     [TestFixture]
     public class TinTests
     {
-        [TestCase("851116-1393", TinCountry.Sweden)]
+        [TestCase("851116-1393", TinCountry.Sweden, ExpectedResult = true)]
+        [TestCase("31046812355", TinCountry.Norway, ExpectedResult = false)]
+        [TestCase("301232-670W", TinCountry.Finland, ExpectedResult = true)]
+        [TestCase("010158-500J", TinCountry.Finland, ExpectedResult = true)]
+        [TestCase("010990-295M", TinCountry.Finland, ExpectedResult = true)]
+        [TestCase("200949-761B", TinCountry.Finland, ExpectedResult = true)]
+        [TestCase("250639-549R", TinCountry.Finland, ExpectedResult = true)]
+        [TestCase("291165-4883", TinCountry.Finland, ExpectedResult = true)]
         public static bool Validate_WithSuppliedValue_ReturnsTrueOrFalse(string value, TinCountry country)
         {
             return value.Validate(country);
